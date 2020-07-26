a1498d7b29643796daf6debab2f7dcd9e0ebda17
DAVVIDSD <david190698@gmail.com>
2020-07-25 21:15:36 -0500

    I,II,III

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -23,13 +23,20 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
- 
+  for ( var i = 0; i < array.length; i++) {
+    array[i]++
+  }
+  return array
 }
 
 function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+   array.push(elemento)
+   return array
+
+  
 }
 
 function agregarItemAlComienzoDelArray(array, elemento) {
@@ -37,6 +44,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift(elemento)
+  return array
 }
 
 function dePalabrasAFrase(palabras) {
@@ -45,30 +54,44 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  return palabras.join(" ")
 }
 
 function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  return array.includes(elemento)
 }
 
 function agregarNumeros(numeros) {
   // "array" debe ser una matriz de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  let suma = 0
+  for(i = 0; i < numeros.length; i++){
+    suma = suma + numeros[i] 
+  }
+  return suma
 }
 
 function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  let total = 0
+  
+  for(i = 0; i < resultadosTest.length; i++){
+    total = total + resultadosTest[i]
+  }
+  return total / resultadosTest.length
 }
 
 function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  return Math.max.apply(null , numeros)
 }
 
 function multiplicarArgumentos() {
@@ -76,6 +99,14 @@ function multiplicarArgumentos() {
   // Si no se pasan argumentos devuelve 0
   // Si se pasa un argumento, simplemente devuélvelo
   // Tu código:
+  if(arguments.length < 1){
+    return 0
+  }
+  let resultado = 1
+  for(i = 0; i < arguments.length; i++){
+    resultado = resultado * arguments[i]
+  }
+  return resultado
 }
 
 // No modificar nada debajo de esta línea
