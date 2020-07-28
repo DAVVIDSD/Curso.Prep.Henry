2731414fbd174034796ee780371a2962a72a7786
DAVVIDSD <david190698@gmail.com>
2020-07-28 16:44:24 -0500

    Ejercicios JS Completados

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -1,24 +1,38 @@
 // Do not change any of the function names
 
+const { RuleTester } = require("eslint");
+
 function invocarCallback(cb) {
   // Invoca al callback `cb`
+  cb();
 }
 
 function sumarArray(numeros, cb) {
   // Suma todos los números enteros (int/integers) de un array ("numeros")
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
+  let total = numeros.reduce(function (acc, index) {
+    return acc + index;
+  }, 0);
+  cb(total);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
+  array.forEach(function (value) {
+    cb(value);
+  });
 }
 
 function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
+  let nuevoArray = array.map(function (value) {
+    return cb(value);
+  });
+  return nuevoArray;
 }
 
 // No modificar nada debajo de esta línea
